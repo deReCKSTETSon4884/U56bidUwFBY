e8ed4a38bf9b087cd4ba94958b6e82baccb63052
deReCKSTETSon4884 <apathy-unison.00@icloud.com>
2025-10-29 04:55:48 +0800

    Create new 设备远程控制 feature

---
+++ remote_control_vue_component_1029_0455_gco.vue
@@ -0,0 +1,33 @@
+// 代码生成时间: 2025-10-29 04:55:48
+import { ref, onMounted, onUnmounted } from 'vue';
+
+// 响应式数据
+const deviceStatus = ref('');
+
+// 模拟发送命令到设备
+const sendCommand = (command) => {
+  console.log(`发送命令 ${command}`);
+  if (command === 'powerOn') {
+    deviceStatus.value = '开机';
+  } else if (command === 'powerOff') {
+    deviceStatus.value = '关机';
+  } else if (command === 'volumeUp') {
+    deviceStatus.value = '音量增加';
+  } else if (command === 'volumeDown') {
+    deviceStatus.value = '音量减少';
+  }
+};
+
+// 生命周期钩子
+onMounted(() => {
+  console.log('组件挂载完毕');
+});
+
+onUnmounted(() => {
+  console.log('组件卸载');
+});
+</script>
+
+<style scoped>
+/* 可以在这里添加组件的样式 */
+</style>
